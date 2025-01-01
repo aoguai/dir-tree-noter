ee9c9c85ec52538ad799d0d50bab50de8c444fed
aoguai <1340219674@qq.com>
2025-01-01 23:00:45 +0800

    build: fix webpack production config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,80 +1,145 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var path = require("path");
-var webpack = require("webpack");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var CleanWebpackPlugin = require("clean-webpack-plugin");
-var CopyWebpackPlugin = require("copy-webpack-plugin");
-var autoprefixer = require("autoprefixer");
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
-var productionConfig = [
-  {
-    entry: "./assets/index",
-    output: {
-      filename: "index.bundle.js",
-      path: path.resolve(__dirname, "public"),
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-          loader: "file?name=fonts/[name].[ext]",
+module.exports = {
+  mode: "production",
+  entry: "./assets/index",
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]",
+          },
         },
-        {
-          test: /\.(png|jpg)$/,
-          loader:
-            "url?limit=8192&context=assets&name=[path][name]_[hash:12].[ext]",
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 8192,
+            context: "assets",
+            name: "[path][name]_[hash:12].[ext]",
+          },
         },
-        {
-          test: /\.scss$/,
-          loader: ExtractTextPlugin.extract(
-            "style",
-            "css!postcss!resolve-url!sass?sourceMap"
-          ),
-        },
-        {
-          test: /\.js$/,
-          loader: "babel",
-          include: path.resolve(__dirname, "assets"),
-        },
-      ],
-    },
-    plugins: [
-      new CleanWebpackPlugin(["public"], {
-        verbose: true,
-        exclude: [".git"],
-      }),
-      new ExtractTextPlugin("index.css", {
-        allChunks: true,
-      }),
-      new webpack.DefinePlugin({
-        "process.env": {
-          NODE_ENV: JSON.stringify("production"),
-        },
-      }),
-      new webpack.optimize.UglifyJsPlugin(),
-      new HtmlWebpackPlugin({
-        template: "./index.ejs",
-      }),
-      new CopyWebpackPlugin([
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      overrideBrowserslist: [
+                        "Android >= 4.0",
+                        "iOS >= 7.0",
+                        "Chrome > 31",
+                        "ff > 31",
+                        "ie >= 10",
+                      ],
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+          {
+            loader: "resolve-url-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        include: path.resolve(__dirname, "assets"),
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["**/*", "!.git/**"],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "index.css",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.ejs",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
         { from: "vendor" },
         { from: "docs", to: "docs" },
         { from: "CNAME" },
         { from: "ie.html" },
         { from: "favicon.ico" },
-      ]),
-    ],
-    postcss: [
-      autoprefixer({
-        browsers: [
-          "Android >= 4.0",
-          "iOS >= 7.0",
-          "Chrome > 31",
-          "ff > 31",
-          "ie >= 10",
-        ],
+      ],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+        },
+        parallel: true,
       }),
     ],
   },
-];
-
-module.exports = productionConfig;
+  devtool: "source-map",
+};
